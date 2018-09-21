ee56700f5b519891771fa695d0d659117e67ddbc
joseluisperez10 <43395219+joseluisperez10@users.noreply.github.com>
2018-09-21 18:41:18 -0500

    Algoritmo2
    Es el algoritmo numero 2 del examen

---
+++ Algoritmo2/Algoritmo2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] Nombre = { "jose", "Luis", "Perez", "Calvo" };
+                foreach (string i in Nombre)
+                {
+                    Console.Write("{0} ", i);
+                }
+                Console.WriteLine();
+                for (int i = 3; i >= 0; i--)
+                {
+                    Console.Write("{0} ", Nombre[i]);
+                }
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
