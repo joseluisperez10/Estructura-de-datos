2efbd94fbd4da6ec6388779eef04d8820a66e05a
joseluisperez10 <43395219+joseluisperez10@users.noreply.github.com>
2018-09-23 22:39:03 -0500

    Algoritmo8
    Es el algoritmo 8 del examen

---
+++ Algoritmo8/Algoritmo8/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i;
+                string a;
+                string caracter;
+                string inverso;
+                inverso = "";
+                do
+                {
+                    Console.Write("introduce 5 digitos: ");
+                    a = Console.ReadLine();
+                    i = a.Length;
+                    if (i != 5)
+                    {
+                        Console.WriteLine("no tiene los digitos necesarios");
+                    }
+                } while (i != 5);
+                for (int k = 4; k >= 0; k--)
+                {
+                    caracter = a.Substring(k, 1); 
+                    inverso = inverso + caracter; ;
+                }                                  
+                if (a == inverso)
+                {
+                    Console.WriteLine("es palindrome");
+                }
+                else
+                {
+                    Console.WriteLine("no es palindrome");
+                }
+
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
