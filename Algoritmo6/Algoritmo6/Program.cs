f8877c2d20fa6ffd32e132ec10e55ef62ca19274
joseluisperez10 <43395219+joseluisperez10@users.noreply.github.com>
2018-09-24 18:31:56 -0500

    Algoritmo6
    Este es el algoritmo 6 del examen

---
+++ Algoritmo6/Algoritmo6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int mayor = 0, posicion = 0;
+                Console.Write("Numero de veces: ");
+                int a = int.Parse(Console.ReadLine());
+                string[] Cadena = new string[a];
+                for(int i=0; i<a;i++)
+                {
+                    Console.Write("palabra {0}: ",(i + 1));
+                    Cadena[i] = Console.ReadLine();
+                    int k = 0;
+                    foreach(char b in Cadena[i])
+                    {
+                        k++;
+                    }
+                    if(k>mayor)
+                    {
+                        mayor = k;
+                        posicion = i ;
+                    }
+                    
+                }
+                Console.WriteLine("la primera ves que la palabra sucesora fue mas alta es: {0} en la posicion: {1}", Cadena[posicion], (posicion +1) );
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
