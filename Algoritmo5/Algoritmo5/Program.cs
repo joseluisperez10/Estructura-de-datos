04c9f47bcc505732b036178713dfcda2c6b633ee
joseluisperez10 <43395219+joseluisperez10@users.noreply.github.com>
2018-09-23 20:49:08 -0500

    Algoritmo5
    Es el algoritmo 5 del examen

---
+++ Algoritmo5/Algoritmo5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("cantidad de numeros: ");
+                int a = int.Parse(Console.ReadLine());
+                int[] arre = new int[a];
+                int mayor = 0;
+                int posicion = 0;
+                for (int i = 0; i < a; i++)
+                {
+                    Console.Write("numero {0}: ", (i + 1));
+                    arre[i] = int.Parse(Console.ReadLine());
+                    if (arre[i] > mayor)
+                    {
+                        mayor = arre[i];
+                        posicion = i + 1;
+                    }
+                }
+                Console.Write("el numero mas grande es: {0} y se encuentra en la posicion: {1}", mayor, posicion);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
