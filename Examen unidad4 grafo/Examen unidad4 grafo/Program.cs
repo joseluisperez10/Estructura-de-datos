807c29d7115752090b5901f52fa2db8262db7015
joseluisperez10 <43395219+joseluisperez10@users.noreply.github.com>
2018-11-23 16:16:01 -0800

    Examenes unidad 4
    Profe este son dos archivos, uno  de los arboles y otro de grafos

---
+++ Examen unidad4 grafo/Examen unidad4 grafo/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_unidad4_grafo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imprimir imprimir = new Imprimir();
+            imprimir.Desplegar();
+            Console.ReadKey();
+
+        }
+    }
+    public class Imprimir
+    {
+        public void Desplegar()
+        {
+            Seleccion:
+            Console.Clear();
+            Console.WriteLine("Seleccione la Ruta que desea mirar:\n1) Boston->LA\n2)NewYork->San Francisco\n3)Atlanta->San Francisco\n4)Denver->NewYork\n5)Salir");
+            int Numerito1 = int.Parse(Console.ReadLine());
+            if (Numerito1 == 1)
+            {
+                Grafo Grafito1 = new Grafo(5);
+                Grafito1.Agregar(0, 1);
+                Grafito1.Agregar(1, 2);
+                Grafito1.Agregar(2, 3);
+                Grafito1.DFS(0, 1);
+                Console.WriteLine("\n");
+                Console.WriteLine("el recorrido es de 2,602 millas");
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Seleccion;
+            }
+
+            if (Numerito1 == 2)
+            {
+                Grafo Grafito1 = new Grafo(7);
+                Grafito1.Agregar(0, 1);
+                Grafito1.Agregar(1, 2);
+                Grafito1.Agregar(2, 3);
+                Grafito1.Agregar(4, 5);
+                Grafito1.DFS(4, 2);
+                Console.WriteLine("\n");
+                Console.WriteLine("el recorrido es de 2,587 millas");
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Seleccion;
+            }
+
+            if (Numerito1 == 3)
+            {
+                Grafo Grafito1 = new Grafo(4);
+                Grafito1.Agregar(0, 1);
+                Grafito1.Agregar(1, 2);
+                Grafito1.DFS(0, 3);
+                Console.WriteLine("\n");
+                Console.WriteLine("el recorrido es de 2,461 millas");
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Seleccion;
+            }
+
+            if (Numerito1 == 4)
+            {
+                Grafo Grafito1 = new Grafo(4);
+                Grafito1.Agregar(0, 1);
+                Grafito1.Agregar(1, 2);
+                Grafito1.DFS(0, 4);
+                Console.WriteLine("\n");
+                Console.WriteLine("el recorrido es de 1,630 millas");
+                Console.WriteLine("\n\nPresione para continuar...");
+                Console.ReadKey();
+                goto Seleccion;
+            }
+            if (Numerito1 == 5)
+            {
+                Console.Clear();
+                Console.WriteLine("ADIOS");
+                Console.ReadKey();
+            }
+        }
+    }
+    public class Grafo
+    {
+        public int Vertices { get; set; }
+        List<int>[] Lista;
+        public Grafo(int Valor)
+        {
+            Vertices = Valor;
+            Lista = new List<int>[Valor];
+            for (int i = 0; i < Valor; i++)
+            {
+                Lista[i] = new List<int>();
+            }
+        }
+
+        public void Agregar(int Indice, int Valor)
+        {
+            Lista[Indice].Add(Valor);
+        }
+
+
+
+        public void DFS(int Valor, int Numerito1)
+        {
+            List<string> Ciudad = new List<string>();
+            if (Numerito1 == 1 || Numerito1 == 2)
+            {
+                Ciudad.Add("Boston");
+                Ciudad.Add("Chicago");
+                Ciudad.Add("Denver");
+                Ciudad.Add("LA");
+                Ciudad.Add("New York");
+                Ciudad.Add("San Francisco");
+                Ciudad.Add("Miami");
+                Ciudad.Add("Atlanta");
+            }
+            if (Numerito1 == 3)
+            {
+                Ciudad.Add("Atlanta");
+                Ciudad.Add("Chicago");
+                Ciudad.Add("San Francisco");
+                Ciudad.Add("Boston");
+                Ciudad.Add("Denver");
+                Ciudad.Add("LA");
+                Ciudad.Add("New York");
+                Ciudad.Add("Miami");
+            }
+            if (Numerito1 == 4)
+            {
+                Ciudad.Add("Denver");
+                Ciudad.Add("Chicago");
+                Ciudad.Add("New York");
+                Ciudad.Add("Atlanta");
+                Ciudad.Add("San Francisco");
+                Ciudad.Add("Boston");
+                Ciudad.Add("LA");
+                Ciudad.Add("Miami");
+            }
+
+            bool[] Visita = new bool[Vertices];
+            Stack<int> Pila = new Stack<int>();
+            Visita[Valor] = true;
+            Pila.Push(Valor);
+            Console.SetCursorPosition(0, 8);
+            Console.WriteLine("El camino para llegar al ultimo elmento es: ");
+            while (Pila.Count != 0)
+            {
+                Valor = Pila.Pop();
+                Console.Write("{0}-> ", Ciudad[Valor]);
+                foreach (int item in Lista[Valor])
+                {
+                    if (!Visita[item])
+                    {
+                        Visita[item] = true;
+                        Pila.Push(item);
+                    }
+                }
+            }
+            Console.Write("Fin de la ruta");
+        }
+    }
+}
---
+++ Examen_Unidad4/Examen_Unidad4/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Unidad4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arbolbinarioordenado arbol = new Arbolbinarioordenado();//se instancia el objeto de la clase Arbolbinarioordenado
+            arbol.Menu();
+            Console.ReadKey();
+        }
+    }
+    public class Arbolbinarioordenado //se crea una clase de tipo publica llamada Arbolbinarioordenado
+    {
+        public void Imprimir_1()
+        {
+            ArbolBinario arbol = new ArbolBinario(); // crea un objeto arbol de BinaryTree
+
+            arbol.z = new Node("A");
+            arbol.z.Izquierda = new Node("B");
+            arbol.z.Medio = new Node("C");
+            arbol.z.Derecha = new Node("D");
+            arbol.z.Izquierda.Izquierda = new Node("E");
+            arbol.z.Izquierda.Medio = new Node("F");
+            arbol.z.Izquierda.Derecha = new Node("G");
+            arbol.z.Derecha.Izquierda = new Node("H");
+            arbol.z.Derecha.Medio = new Node("I");
+            arbol.z.Derecha.Derecha = new Node("J");
+            arbol.z.Izquierda.Izquierda.Izquierda = new Node("K");
+            arbol.z.Izquierda.Izquierda.Medio = new Node("L");
+            arbol.z.Izquierda.Izquierda.Derecha = new Node("M");
+            arbol.z.Izquierda.Derecha.Medio = new Node("N");
+            arbol.z.Derecha.Izquierda.Medio = new Node("O");
+            arbol.z.Derecha.Derecha.Izquierda = new Node("P");
+            arbol.z.Derecha.Derecha.Derecha = new Node("Q");
+            arbol.z.Izquierda.Derecha.Medio.Izquierda = new Node("R");
+            arbol.z.Izquierda.Derecha.Medio.Derecha = new Node("S");
+
+            Console.WriteLine("Arbol A");
+            Console.WriteLine("Recorrido preorden " + "del arbol es ");
+            arbol.PrintPreorder();
+            Console.WriteLine();
+            Console.WriteLine("\nRecorrido posorden " + "del arbol es ");
+            arbol.PrintPostorder();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public void Imprimir()
+        {
+            ArbolBinario arbol = new ArbolBinario(); // crea un objeto arbol de BinaryTree
+
+            arbol.z = new Node("A");
+            arbol.z.Izquierda = new Node("B");
+            arbol.z.Derecha = new Node("E");
+            arbol.z.Izquierda.Izquierda = new Node("C");
+            arbol.z.Izquierda.Derecha = new Node("D");
+            arbol.z.Derecha.Derecha = new Node("F");
+            arbol.z.Derecha.Derecha.Izquierda = new Node("G");
+            arbol.z.Derecha.Derecha.Derecha = new Node("H");
+
+            Console.WriteLine("\nArbol B");
+            Console.WriteLine("Recorrido preorden " + "del arbol es ");
+            arbol.PrintPreorder();
+            Console.WriteLine();
+            Console.WriteLine("\nRecorrido posorden " + "del arbol es ");
+            arbol.PrintPostorder();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public class Node
+        {
+            public string x;
+            public Node Izquierda, Derecha, Medio;
+
+            public Node(string y)
+            {
+                x = y;
+                Izquierda = Derecha = Medio = null;
+            }
+        }
+
+        public class ArbolBinario
+        {
+            public Node z;
+            public ArbolBinario()
+            {
+                z = null;
+            }
+            public void PostOrden(Node node)
+            {
+                if (node == null)
+                    return;
+
+                PostOrden(node.Izquierda);
+
+                PostOrden(node.Medio);
+
+
+                PostOrden(node.Derecha);
+
+
+                Console.Write(node.x + " ");
+            }
+            public void PreOrden(Node node)
+            {
+                if (node == null)
+                    return;
+
+                Console.Write(node.x + " ");
+
+                PreOrden(node.Izquierda);
+
+                PreOrden(node.Medio);
+
+                PreOrden(node.Derecha);
+            }
+            public void PrintPostorder() { PostOrden(z); }
+            public void PrintPreorder() { PreOrden(z); }
+        }
+        public void Menu()
+        {
+            int Menu = 0;
+            do
+            {
+                Console.Clear();
+                Console.Write("" +
+                    "\n1.-Arbol A" +
+                    "\n2.-Arbol B" +
+                    "\n3.-Salir" +
+                    "\n\nElige una opcion: ");
+                Menu = Convert.ToInt16(Console.ReadLine());
+                switch (Menu)
+                {
+                    case 1:
+                        Console.Clear();
+                        Imprimir_1();
+                        Console.ReadKey();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Imprimir();
+                        Console.ReadKey();
+                        break;
+                    case 3:
+                        Console.WriteLine("Presione <Enter> para salir...");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (Menu != 3);
+        }
+    }
+}
